62fca35ff865f30c3bdb9b59bac199bec77a4096
lgzkd1990 <wodeshijie1234@126.com>
2019-06-25 17:18:57 +0800

    第五次提交

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,128 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/login" v-if="!isLogin">Login</router-link>
-      <a @click="logout" v-if="isLogin">Logout</a>
+    <div id="app">
+        <!--    <div id="nav">
+              <router-link to="/">Home</router-link> |
+              <router-link to="/about">About</router-link> |
+              <router-link to="/login" v-if="!isLogin">Login</router-link>
+              <a @click="logout" v-if="isLogin">Logout</a>
+            </div>-->
+        <router-view/>
+        <cube-tab-bar
+                v-model="selectLabel"
+                @change="changeHandler">
+            <cube-tab v-for="(item, index) in tabs" :key="index"
+                      :icon="item.icon" :label="item.value">
+                <span>{{item.label}}</span>
+                <span class="badge" v-if="showBadge(item.label)">{{cartTotal}}</span>
+            </cube-tab>
+        </cube-tab-bar>
     </div>
-    <router-view/>
-  </div>
 </template>
 <script>
-import { mapGetters} from 'vuex';
+    import {mapGetters} from 'vuex';
 
-export default {
-  methods:{
-    logout(){
-      this.$http.get('/api/logout');
+    export default {
+        data() {
+            return {
+                selectLbale: "/",
+                tabs: [
+                    {label: "Home", value: "/", icon: "cubeic-home"},
+                    {label: "Cart", value: "/cart", icon: "cubeic-mall"},
+                    {label: "Me", value: "/login", icon: "cubeic-person"}
+                ]
+            };
+        },
+        watch: {
+            // 路由发生变化时，同步tabs选中
+            $route(route) {
+                this.selectLabel = route.path;
+            }
+        },
+        created() {
+            // 初始化页签设置
+            this.selectLabel = this.$route.path;
+        },
+        methods: {
+            logout() {
+                this.$http.get('/api/logout');
+            },
+            changeHandler(val) {
+                this.$router.push(val);
+            },
+            showBadge(label) {
+                return label == "Cart" && this.cartTotal > 0;
+            }
+        },
+        computed: {
+            ...mapGetters(['isLogin'])
+        },
     }
-  },
-computed: {
- ...mapGetters(['isLogin'])
-},
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    #nav {
+        padding: 30px;
+    }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
+
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    }
+
+    .cube-tab-bar {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: #edf0f4;
+    }
+
+    .cube-tab-bar-slider {
+        top: 0;
+    }
+
+    /* 页面滑动动画 */
+    /* 入场前 */
+    .route-move-enter {
+        transform: translate3d(-100%, 0, 0);
+    }
+
+    /* 出场后 */
+    .route-move-leave-to {
+        transform: translate3d(100%, 0, 0);
+    }
+
+    .route-move-enter-active,
+    .route-move-leave-active {
+        transition: transform 0.3s;
+    }
+
+    .child-view {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        padding-bottom: 36px;
+    }
+
+    span.badge {
+        display: inline-block;
+        background: #de3529;
+        color: white;
+        width: 1rem;
+        height: 1rem;
+        border-radius: 50%;
+    }
 </style>
